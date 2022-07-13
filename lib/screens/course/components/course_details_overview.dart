f2cfa3b378e87e4eb3a6414b318e3348a3af900e
Karlis Berzins <berzinsh.karlis@gmail.com>
2022-07-13 08:37:34 +0300

    Add buy course button

--- lib/screens/course/components/course_details_overview.dart
+++ lib/screens/course/components/course_details_overview.dart
@@ -12,47 +12,50 @@ class CourseDetailsOverview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Do you want to move your career to UI/UX Designer and start from scratch to advanced? Do you want to be a UI/UX Designer that is ready for work? Are you a UI/UX Designer who wants to add skills? This class suits you!',
-          style: TextStyles.kTextXsRegular
-              .withColor(AppColors.kBrandColorAccentBlack),
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 16),
-          child: CourseDetailsOverviewTags(),
-        ),
-        Text(
-          'Last Updated : January 2022',
-          style: TextStyles.kTextXsRegular
-              .withColor(AppColors.kBrandColorAccentBlack),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16),
-          child: Text(
-            'Subtitle : Yes',
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 56),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Do you want to move your career to UI/UX Designer and start from scratch to advanced? Do you want to be a UI/UX Designer that is ready for work? Are you a UI/UX Designer who wants to add skills? This class suits you!',
             style: TextStyles.kTextXsRegular
                 .withColor(AppColors.kBrandColorAccentBlack),
           ),
-        ),
-        Text(
-          'In this class, you will study UI/UX Design from the beginning to being a proficient and working project to make you as UI/UX Designer ready to work.\n\n' +
-              'In this class, we will work on a case study Online Class Mobile Application, in learning all the essential elements inside UI/UX Design.\n\n' +
-              'After studying this class, you will be able to make:\n\n' +
-              '1. Design Thinking\n' +
-              '2. User Flow\n' +
-              '3. Wireframe\n' +
-              '4. UI Style Guide\n' +
-              '5. UI/UX Design\n' +
-              '6. UI/UX Prototype\n' +
-              '7. User Research Documents\n' +
-              '8. UX Case Study\n',
-          style: TextStyles.kTextXsRegular
-              .withColor(AppColors.kBrandColorAccentBlack),
-        ),
-      ],
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 16),
+            child: CourseDetailsOverviewTags(),
+          ),
+          Text(
+            'Last Updated : January 2022',
+            style: TextStyles.kTextXsRegular
+                .withColor(AppColors.kBrandColorAccentBlack),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: Text(
+              'Subtitle : Yes',
+              style: TextStyles.kTextXsRegular
+                  .withColor(AppColors.kBrandColorAccentBlack),
+            ),
+          ),
+          Text(
+            'In this class, you will study UI/UX Design from the beginning to being a proficient and working project to make you as UI/UX Designer ready to work.\n\n' +
+                'In this class, we will work on a case study Online Class Mobile Application, in learning all the essential elements inside UI/UX Design.\n\n' +
+                'After studying this class, you will be able to make:\n\n' +
+                '1. Design Thinking\n' +
+                '2. User Flow\n' +
+                '3. Wireframe\n' +
+                '4. UI Style Guide\n' +
+                '5. UI/UX Design\n' +
+                '6. UI/UX Prototype\n' +
+                '7. User Research Documents\n' +
+                '8. UX Case Study\n',
+            style: TextStyles.kTextXsRegular
+                .withColor(AppColors.kBrandColorAccentBlack),
+          ),
+        ],
+      ),
     );
   }
 }
