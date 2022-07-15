a27bc4dae249c1f0f9dc9a0e6c9ad75dc1df5874
Karlis Berzins <berzinsh.karlis@gmail.com>
2022-07-15 05:41:39 +0300

    Finish UI for Material section

--- lib/screens/course/components/material/course_details_material.dart
+++ lib/screens/course/components/material/course_details_material.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../../../resources/constants/colors.dart';
+import '../../../../resources/constants/text_styles.dart';
+import '../../../../resources/extensions/text_style.dart';
 import './course_details_material_row.dart';
 
 class CourseDetailsMaterial extends StatelessWidget {
@@ -11,12 +13,52 @@ class CourseDetailsMaterial extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all(
-            color: AppColors.kBrandColorPrimary,
-          )),
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: AppColors.kBrandColorPrimary,
+        ),
+      ),
       width: MediaQuery.of(context).size.width,
-      child: CourseDetailsMaterialRow(),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Introduction to UX',
+                    style: TextStyles.kTextMdBold
+                        .withColor(AppColors.kBrandColorAccentBlack),
+                  ),
+                  Text(
+                    '5 videos',
+                    style: TextStyles.kTextSmRegular,
+                  ),
+                ],
+              ),
+              const Image(
+                image: AssetImage('asset/images/icon_arrow_down.png'),
+              ),
+            ],
+          ),
+          Container(
+            height: 380,
+            padding: const EdgeInsets.fromLTRB(8, 16, 8, 0),
+            child: ListView.separated(
+              itemBuilder: ((context, index) {
+                return const CourseDetailsMaterialRow();
+              }),
+              separatorBuilder: ((context, index) {
+                return const SizedBox(height: 16);
+              }),
+              itemCount: 5,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
