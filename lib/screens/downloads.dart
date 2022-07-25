07bbcf1fdce5d495987d800b138eca37a8043607
Karlis Berzins <berzinsh.karlis@gmail.com>
2022-07-25 06:21:06 +0300

    Finish UI for downloads page

--- lib/screens/downloads.dart
+++ lib/screens/downloads.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../resources/constants/colors.dart';
 import '../resources/constants/text_styles.dart';
 import '../resources/extensions/text_style.dart';
+import '../components/primary_button.dart';
 
 class Downloads extends StatelessWidget {
   const Downloads({Key? key}) : super(key: key);
@@ -47,27 +48,48 @@ class Downloads extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'UX/UI Design Course 2022',
-                        style: TextStyles.kTextSmBold
-                            .withColor(AppColors.kBrandColorAccentBlack),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 16),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [Text('Maja Indira'), Text('100 MB')],
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.472,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'UX/UI Design Course 2022',
+                          style: TextStyles.kTextSmBold
+                              .withColor(AppColors.kBrandColorAccentBlack),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Maja Indira',
+                                style: TextStyles.kTextXsRegular
+                                    .withColor(AppColors.kNeutralColor500),
+                              ),
+                              Text(
+                                '100 MB',
+                                style: TextStyles.kTextXsBold
+                                    .withColor(AppColors.kNeutralColor500),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   const Image(
                     image: AssetImage('asset/images/icon_delete.png'),
                   ),
                 ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: PrimaryButton(
+                title: 'Find more to download',
+                onClick: () {},
               ),
             ),
           ],
