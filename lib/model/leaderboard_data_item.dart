5bfba788bb86318689e7bbf4706f6cf986c0b28a
Karlis Berzins <berzinsh.karlis@gmail.com>
2022-08-01 07:42:17 +0300

    Update Leaderboard screen to render list with items

---
+++ lib/model/leaderboard_data_item.dart
@@ -0,0 +1,13 @@
+class LeaderboardDataItem {
+  final String imageName;
+  final String username;
+  final int score;
+  final int rank;
+
+  const LeaderboardDataItem({
+    required this.imageName,
+    required this.username,
+    required this.score,
+    required this.rank,
+  });
+}
--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -2,9 +2,51 @@ import 'package:flutter/material.dart';
 
 import './leaderboard/leaderboard_header.dart';
 import './leaderboard/leaderboard_row_item.dart';
+import '../model/leaderboard_data_item.dart';
+
+const leaderboardData = [
+  LeaderboardDataItem(
+    imageName: 'normal_karlis_berzins',
+    username: 'berzinsk',
+    score: 5300,
+    rank: 1,
+  ),
+  LeaderboardDataItem(
+    imageName: 'normal_karlis_berzins',
+    username: 'azizsuzy',
+    score: 5002,
+    rank: 2,
+  ),
+  LeaderboardDataItem(
+    imageName: 'normal_karlis_berzins',
+    username: 'saraaurea',
+    score: 4879,
+    rank: 3,
+  ),
+  LeaderboardDataItem(
+    imageName: 'normal_karlis_berzins',
+    username: 'siskosofia',
+    score: 4735,
+    rank: 4,
+  ),
+  LeaderboardDataItem(
+    imageName: 'normal_karlis_berzins',
+    username: 'annettdejana',
+    score: 4673,
+    rank: 5,
+  ),
+  LeaderboardDataItem(
+    imageName: 'normal_karlis_berzins',
+    username: 'kelseykaleb',
+    score: 4543,
+    rank: 6,
+  ),
+];
 
 class Leaderboard extends StatelessWidget {
-  const Leaderboard({Key? key}) : super(key: key);
+  final bottomList = leaderboardData.where((e) => e.rank > 3).toList();
+
+  Leaderboard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,33 +57,25 @@ class Leaderboard extends StatelessWidget {
           padding: EdgeInsets.only(bottom: 20),
           child: LeaderboardHeader(),
         ),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: const [
-              LeaderboardRowItem(
-                imageName: 'normal_karlis_berzins',
-                username: 'berzinsk',
-                score: 5002,
-                rank: 4,
-              ),
-              SizedBox(height: 16),
-              LeaderboardRowItem(
-                imageName: 'normal_karlis_berzins',
-                username: 'annettdejana',
-                score: 4673,
-                rank: 5,
-              ),
-              SizedBox(height: 16),
-              LeaderboardRowItem(
-                imageName: 'normal_karlis_berzins',
-                username: 'kelseykaleb',
-                score: 4543,
-                rank: 6,
-              ),
-            ],
+        Expanded(
+          child: ListView.separated(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 32),
+            itemBuilder: (context, index) {
+              LeaderboardDataItem item = bottomList[index];
+
+              return LeaderboardRowItem(
+                imageName: item.imageName,
+                username: item.username,
+                score: item.score,
+                rank: item.rank,
+              );
+            },
+            itemCount: bottomList.length,
+            separatorBuilder: (context, index) => const SizedBox(
+              height: 16,
+            ),
           ),
-        )
+        ),
       ],
     );
   }
